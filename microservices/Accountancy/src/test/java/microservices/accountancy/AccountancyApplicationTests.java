1f754f75df5a3f1bbd49b6764c793875bb9b2a03
mamoraw <144896087+mamoraw@users.noreply.github.com>
2024-09-06 18:58:24 +0200

    added accountancy module

---
+++ microservices/Accountancy/src/test/java/microservices/accountancy/AccountancyApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.accountancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
